83ecb2c2d3cc67f2e992998b1a7d2660ee245daa
Pc Elementary Os 2021 <bubu@outlook.com.pe>
2021-03-13 05:09:26 -0500

    Como mostras html con las vistas

---
+++ resources/views/home.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Home</title>
+</head>
+
+<body>
+<nav>
+    <ul>
+        <li><a href="/">Home</a></li>
+        <li><a href="/about">About</a></li>
+        <li><a href="/portafolio"></a>Portafolio</li>
+        <li><a href="/contact"></a>Contact</li>
+    </ul>
+</nav>
+
+<h1>Home</h1>
+Bienvenido <?php echo $nombre ?? "Invitado" ?>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -12,20 +12,10 @@
 */
 
 
-#====================================
-#   Rutas con nombres
-#   contactame, es la url
-#   contactos, es el nombre de la ruta
-#------------------------------------
-Route::get('contactame', function () {
-    return 'seccion de contactos';
-})->name('contactos');
 
-Route::get('/', function () {
-    echo "<a href='" . route('contactos') . "'>Contactos 1</a><br>";
-    echo "<a href='" . route('contactos') . "'>Contactos 2</a><br>";
-    echo "<a href='" . route('contactos') . "'>Contactos 3</a><br>";
-    echo "<a href='" . route('contactos') . "'>Contactos 4</a><br>";
-    echo "<a href='" . route('contactos') . "'>Contactos 5</a><br>";
-});
+Route::view('/', 'home')->name('home');
+Route::view('/about', 'about')->name('about');
+Route::view('/portafolio', 'portafolio')->name('portafolio');
+Route::view('/contact', 'contact')->name('contact');
+
 
