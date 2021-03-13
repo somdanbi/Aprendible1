a226afe7b34fbbf8b42332b144a12212c45d1e2c
Pc Elementary Os 2021 <bubu@outlook.com.pe>
2021-03-13 03:57:58 -0500

    Saludo con parametro obligatorio

--- routes/web.php
+++ routes/web.php
@@ -8,9 +8,20 @@
 | Here is where you can register web routes for your application. These
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
-|
+| son las url de entradas de la aplicacion.
 */
 
+
+
+# Peticion Get y la funcion tambien conocida como closure
+# definimos la Url y respondemos con una funcion
 Route::get('/', function () {
-    return view('welcome');
+    return 'hola desde la pagina de inicio';
 });
+
+#------------------------------------
+# Saludos con parametro obligatorio
+Route::get('/saludo/{nombre}',function($nombre){
+    return 'hola ' . $nombre;
+});
+
