69d30cf19951367823e6b21d80c2daa01c253dee
Pc Elementary Os 2021 <bubu@outlook.com.pe>
2021-03-16 09:27:23 -0500

    Sin importancia

--- resources/views/portafolio.blade.php
+++ resources/views/portafolio.blade.php
@@ -1,8 +1,19 @@
-@extends('layout')
-
+@extends('layouts.app')
 @section('title','Portafolio')
-
-    
 @section('content')
-    <h1>Portafolio</h1>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">Portafolio</div>
+
+                    <div class="card-body">
+
+
+                        You are logged in!
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
