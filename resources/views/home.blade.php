6665bb16abf4d4f3e20fded0c10775353396e858
Pc Elementary Os 2021 <bubu@outlook.com.pe>
2021-03-16 12:49:38 -0500

    Listing with Foreach

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,10 +8,61 @@
             <!-- Title & Breadcrumbs-->
             <div class="row page-titles">
                 <div class="col-md-12 align-self-center">
-                    <h4 class="theme-cl">Inicio</h4>
+                    <h4 class="theme-cl">Lista de Todos los Usuarios</h4>
                 </div>
             </div>
             <!-- Title & Breadcrumbs-->
+
+
+            <!-- Main content -->
+            <section class="content">
+
+                <!-- Default box -->
+                <div class="box">
+
+                    <!-- /.box-header -->
+                    <div class="box-body">
+                        <div class="form-group">
+                            <a href="#" class="btn btn-success">
+                                Crear Usuario
+                            </a>
+                        </div>
+
+
+                        <table id="example1" class="table table-bordered table-striped">
+                            <thead>
+                            <tr>
+                                <th>ID</th>
+                                <th>Nombre</th>
+                                <th>Foto</th>
+                                <th>E-mail</th>
+
+                            </tr>
+                            </thead>
+                            <tbody>
+                            @foreach($users as $user)
+                                <tr>
+                                    <td>{{$user->id}}</td>
+                                    <td>{{$user->name}}</td>
+                                    <td>
+                                        <img src="{{asset('img/no-avatar.svg')}}" alt="" class="img-responsive" width="60" height="50">
+                                    </td>
+                                    <td>{{$user->email}}</td>
+
+
+                                </tr>
+                            @endforeach
+
+                            </tbody>
+                        </table>
+                    </div>
+                    <!-- /.box-body -->
+                </div>
+                <!-- /.box -->
+
+            </section>
+            <!-- /.content -->
+
 
 
         </div>
